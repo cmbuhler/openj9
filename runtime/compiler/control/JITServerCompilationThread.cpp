9e1e140d91595fffd499294bf0a05a80f2301972
XuechunHou <xhou1@ualberta.ca>
2020-01-25 15:19:57 -0700

    retrieved method signiture

--- runtime/compiler/control/JITServerCompilationThread.cpp
+++ runtime/compiler/control/JITServerCompilationThread.cpp
@@ -89,11 +89,25 @@ outOfProcessCompilationEnd(
    if (comp-> getOption(TR_PersistLogging)) {
       // insert persistent logging implementation
       uint64_t clientUID = entry->getClientUID();
-      const char* methodSignature = compInfoPT->getCompilation()->signature();
+      char* methodClassName = compInfoPT->getCompilation()->getMethodBeingCompiled()->classNameChars();
+      char* methodName = compInfoPT->getCompilation()->getMethodBeingCompiled()->nameChars();
+      char* methodSigniture = compInfoPT->getCompilation()->getMethodBeingCompiled()->signatureChars();
+      
 
+      uint16_t methodClassNameLength = compInfoPT->getCompilation()->getMethodBeingCompiled()->classNameLength();
+      uint16_t methodNameLength = compInfoPT->getCompilation()->getMethodBeingCompiled()->nameLength();
+      uint16_t methodSignitureLength = compInfoPT->getCompilation()->getMethodBeingCompiled()->signatureLength();
+      // need the client id 
+      // and the method name 
       std::cout << "Persistent Logging enabled" << std::endl;
       std::cout << "Found Client id: " << clientUID << std:: endl;
-      printf("potential method full name: %s\n",methodSignature);
+      // By default, regular arrays of local scope (for example, those declared within a function) are left uninitialized. 
+      char methodFullName[methodClassNameLength + methodNameLength + methodSignitureLength + 2] = {'\0'};
+      std::strncat(methodFullName, methodClassName, methodClassNameLength);
+      std::strcat(methodFullName, ".");
+      std::strncat(methodFullName, methodName, methodNameLength);
+      std::strncat(methodFullName, methodSigniture, methodSignitureLength);
+      printf("potential method full name: %s\n",methodFullName);
       uint32_t persistentLoggingDatabasePort = compInfoPT->getCompilationInfo()->getPersistentInfo()->getJITServerPersistentLoggingDatabasePort();
       std::cout << "what is the persistent logging database port ? " << persistentLoggingDatabasePort << std::endl;
    }
