b48c9e2405a9f607ad51a9ac6e8ee9b7fb6ffbdb
Chase Buhler <cmbuhler@ualberta.ca>
2020-02-27 22:19:31 -0700

    Fix an issue with my previous merge

--- runtime/compiler/env/j9methodServer.cpp
+++ runtime/compiler/env/j9methodServer.cpp
@@ -2308,24 +2308,6 @@ TR_ResolvedRelocatableJ9JITServerMethod::definingClassFromCPFieldRef(TR::Compila
    return resolvedClass;
    }
 
-TR_OpaqueClassBlock * 
-TR_ResolvedRelocatableJ9JITServerMethod::definingClassFromCPFieldRef(TR::Compilation *comp, int32_t cpIndex, bool isStatic)
-   {
-   TR_OpaqueClassBlock *resolvedClass = TR_ResolvedJ9JITServerMethod::definingClassFromCPFieldRef(comp, cpIndex, isStatic);
-   if (resolvedClass)
-      {
-      bool valid = false;
-      if (comp->getOption(TR_UseSymbolValidationManager))
-         valid = comp->getSymbolValidationManager()->addDefiningClassFromCPRecord(resolvedClass, cp(), cpIndex, isStatic);
-      else
-         valid = storeValidationRecordIfNecessary(comp, cp(), cpIndex, isStatic ? TR_ValidateStaticField : TR_ValidateInstanceField, ramMethod());
-
-      if (!valid)
-         resolvedClass = NULL;
-      }
-   return resolvedClass;
-   }
-
 TR_OpaqueClassBlock *
 TR_ResolvedRelocatableJ9JITServerMethod::getClassFromConstantPool(TR::Compilation *comp, uint32_t cpIndex, bool returnClassForAOT)
    {
